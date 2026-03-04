b24b6f2b68341fc0d89146cfc7c3da6ef337c24c
zapshooter <92568875+zapshooter@users.noreply.github.com>
2026-03-04 22:05:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const byteSize = (str) => {
-  // write your code here
+  return new Blob([str]).size;
 };
-
 // Do not change the code below
 const str = prompt("Enter some string.");
 alert(byteSize(str));
